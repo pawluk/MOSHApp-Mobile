4ee90df7443ce74c88e07ce33648172760878e3e
Jason Recillo <jaysan1292@gmail.com>
2013-05-07 01:01:59 -0400

    Fixed jslint errors and reformatted code

--- Web App/www/js/audioplay.js
+++ Web App/www/js/audioplay.js
@@ -1,85 +1,94 @@
-function initilizepage(){
-var taskdic=[{image:"noimage.png",audio:"sample.mp3",desc:"First, Will be the text version of audio"},{image:"noimage.png",audio:"sample1.mp3",desc:"Second, Will be the text version of audio"},
-{image:"noimage.png",audio:"sample2.mp3",desc:"Third, Will be the text version of audio"}];
-for(var i=0;i<window.localStorage.getItem("numberofscript");i++){
-	if(i===0)
-	$('#imgul').append('<li style="display:block"><div><img src="img/'+window.localStorage.getItem(("image"+i))+'" width="320px" height="180px"/></div></li>');
-	else
-	$('#imgul').append('<li style="display:none"><div><img src="img/'+window.localStorage.getItem(("image"+i))+'" width="320px" height="180px"/></div></li>');
-}
-			$('#audioinfo').html(window.localStorage.getItem("audio0"));
-			$('#desc').html(window.localStorage.getItem("scripttext0"));
-			var audname=window.localStorage.getItem("audio0");
-			audname=audname.substring(0,audname.length-4);
-			$('<source id="typeacc" src="http://moshapp.kaldim.com/pages/accs/'+audname+'.m4a"><source id="typeogg" src="http://moshapp.kaldim.com/pages/ogg/'+audname+'.ogg"><source id="typemp3" src="http://moshapp.kaldim.com/pages/mp3s/'+audname+'.mp3"><source id="typewav" src="http://moshapp.kaldim.com/pages/wavs/'+audname+'.wav">').appendTo('#moshplayer');
+function initilizepage() {
+	var taskdic = [{
+		image: "noimage.png",
+		audio: "sample.mp3",
+		desc: "First, Will be the text version of audio"
+	}, {
+		image: "noimage.png",
+		audio: "sample1.mp3",
+		desc: "Second, Will be the text version of audio"
+	}, {
+		image: "noimage.png",
+		audio: "sample2.mp3",
+		desc: "Third, Will be the text version of audio"
+	}];
+	for (var i = 0; i < window.localStorage.getItem("numberofscript"); i++) {
+		if (i === 0) $('#imgul').append('<li style="display:block"><div><img src="img/' + window.localStorage.getItem(("image" + i)) + '" width="320px" height="180px"/></div></li>');
+		else $('#imgul').append('<li style="display:none"><div><img src="img/' + window.localStorage.getItem(("image" + i)) + '" width="320px" height="180px"/></div></li>');
+	}
+	$('#audioinfo').html(window.localStorage.getItem("audio0"));
+	$('#desc').html(window.localStorage.getItem("scripttext0"));
+	var audname = window.localStorage.getItem("audio0");
+	audname = audname.substring(0, audname.length - 4);
+	$('<source id="typeacc" src="http://moshapp.kaldim.com/pages/accs/' + audname + '.m4a"><source id="typeogg" src="http://moshapp.kaldim.com/pages/ogg/' + audname + '.ogg"><source id="typemp3" src="http://moshapp.kaldim.com/pages/mp3s/' + audname + '.mp3"><source id="typewav" src="http://moshapp.kaldim.com/pages/wavs/' + audname + '.wav">').appendTo('#moshplayer');
 	var slider = new Swipe(document.getElementById('imgslider'), {
-	      callback: function(e, pos) {
-	    	  //will added for native version
-			  //stopAudio();
-	        var i = bullets.length;
-	        while (i--) {
-	          bullets[i].className = ' ';
-	        }
-	        bullets[pos].className = 'on';
-			$('#audioinfo').html(window.localStorage.getItem(("audio"+pos)));
-			$('#desc').html(window.localStorage.getItem(("scripttext")+pos));
-			audname=window.localStorage.getItem(("audio"+pos));
-			audname=audname.substring(0,audname.length-4);
+		callback: function(e, pos) {
+			//will added for native version
+			//stopAudio();
+			var i = bullets.length;
+			while (i--) {
+				bullets[i].className = ' ';
+			}
+			bullets[pos].className = 'on';
+			$('#audioinfo').html(window.localStorage.getItem(("audio" + pos)));
+			$('#desc').html(window.localStorage.getItem(("scripttext") + pos));
+			audname = window.localStorage.getItem(("audio" + pos));
+			audname = audname.substring(0, audname.length - 4);
 			$('#moshplayer')[0].pause();
-			$('#typeacc').attr('src','http://moshapp.kaldim.com/pages/accs/'+audname+'.m4a');
-			$('#typemp3').attr('src','http://moshapp.kaldim.com/pages/mp3s/'+audname+'.mp3');
-			$('#typeogg').attr('src','http://moshapp.kaldim.com/pages/ogg/'+audname+'.ogg');
-			$('#typewav').attr('src','http://moshapp.kaldim.com/pages/ogg/'+audname+'.wav');
+			$('#typeacc').attr('src', 'http://moshapp.kaldim.com/pages/accs/' + audname + '.m4a');
+			$('#typemp3').attr('src', 'http://moshapp.kaldim.com/pages/mp3s/' + audname + '.mp3');
+			$('#typeogg').attr('src', 'http://moshapp.kaldim.com/pages/ogg/' + audname + '.ogg');
+			$('#typewav').attr('src', 'http://moshapp.kaldim.com/pages/ogg/' + audname + '.wav');
 			$('#moshplayer')[0].load();
 			$('#play .ui-btn-text').html('Play');
 			$('#play .ui-icon').addClass('ui-icon-audio-play').removeClass('ui-icon-audio-pause');
-	      }
-	    }),
-	    bullets = document.getElementById('position').getElementsByTagName('em');
-	
-	$('#prev').click(function(){
+		}
+	}),
+		bullets = document.getElementById('position').getElementsByTagName('em');
+
+	$('#prev').click(function() {
 		//will added for native version
 		//stopAudio();
-		slider.prev();return false;
-	});
-	
-	$('#next').click(function(){
-		//will added for native version
-		//stopAudio();
-		slider.next();return false;
+		slider.prev();
+		return false;
 	});
 
-	for(var i=0;i<numberofimages;i++){
-		if(i===0)
-			$('#position').append('<em class="on">&bull;</em>');
-		else
-			$('#position').append('<em>&bull;</em>');
+	$('#next').click(function() {
+		//will added for native version
+		//stopAudio();
+		slider.next();
+		return false;
+	});
+
+	for (i = 0; i < numberofimages; i++) {
+		if (i === 0) $('#position').append('<em class="on">&bull;</em>');
+		else $('#position').append('<em>&bull;</em>');
 	}
 
 	var play_btn = $('#play');
 	var stop_btn = $('#stop');
 
-	play_btn.click(function(){
+	play_btn.click(function() {
 		//will added for native version
 		//playAudio(audname);
-		if($('#play .ui-btn-text').html()=='Play'){
-		$('#moshplayer')[0].play();
-		$('#slider').attr( 'max', Math.round($('#moshplayer')[0].duration));
-		$('#slider').slider('refresh');
-		$('#play .ui-btn-text').html('Pause');
-		$('#play .ui-icon').addClass('ui-icon-audio-pause').removeClass('ui-icon-audio-play');
-		}else{
-		$('#moshplayer')[0].pause();
-		$('#play .ui-btn-text').html('Play');
-		$('#play .ui-icon').addClass('ui-icon-audio-play').removeClass('ui-icon-audio-pause');
+		if ($('#play .ui-btn-text').html() == 'Play') {
+			$('#moshplayer')[0].play();
+			$('#slider').attr('max', Math.round($('#moshplayer')[0].duration));
+			$('#slider').slider('refresh');
+			$('#play .ui-btn-text').html('Pause');
+			$('#play .ui-icon').addClass('ui-icon-audio-pause').removeClass('ui-icon-audio-play');
+		} else {
+			$('#moshplayer')[0].pause();
+			$('#play .ui-btn-text').html('Play');
+			$('#play .ui-icon').addClass('ui-icon-audio-play').removeClass('ui-icon-audio-pause');
 		}
 		// $(this).button('disable');
 	});
-	
-	stop_btn.click(function(){
+
+	stop_btn.click(function() {
 		//if(playing){
 		//will added for native version
-		//stopAudio();  
+		//stopAudio();
 		// reset slider
 		$('#moshplayer')[0].load();
 		$('#play .ui-btn-text').html('Play');
@@ -88,24 +97,22 @@ for(var i=0;i<window.localStorage.getItem("numberofscript");i++){
 		//$('#slider').slider('refresh');
 		//}
 	});
-	
+
 }
 
-function updateslider(){
-$('#slider').val(Math.round($('#moshplayer')[0].currentTime));
-$('#slider').slider('refresh');
+function updateslider() {
+	$('#slider').val(Math.round($('#moshplayer')[0].currentTime));
+	$('#slider').slider('refresh');
 }
 
 //will added for native version
 // function getPhoneGapPath() {
 
-    // var path = window.location.pathname;
-    // path = path.substr( path, path.length - 11 );
-    // return 'file://' + path;
+// var path = window.location.pathname;
+// path = path.substr( path, path.length - 11 );
+// return 'file://' + path;
 
 // };
-
-
 
 
 
@@ -113,77 +120,76 @@ $('#slider').slider('refresh');
 // var audio = null;
 // var audioTimer = null;
 // var pausePos = 0;
- //var playing=false;
+//var playing=false;
 
 // /* play audio file */
 // function playAudio(file){
-	// var loctofl=getPhoneGapPath() + file;
-	// audio = new Media(loctofl, function(){ // success callback
-		// $('#play').button('enable'); 
-    	// console.log("playAudio():Audio Success");
-    // }, function(error){ // error callback
-    	// alert('code: '    + error.code    + '\n' + 
-          	  // 'message: ' + error.message + '\n');
-    // },function(status){
-    	// if(status==2)
-    		// playing=true;
-    	// else
-    		// playing=false;
-    // });
-	
-    
-	// var counter = 0;
-    // var timerDur = setInterval(function() {
-        // counter = counter + 100;
-        // if (counter > 2000) {
-            // clearInterval(timerDur);
-        // }
-        // var dur = audio.getDuration();
-        // if (dur > 0) {
-            // clearInterval(timerDur);
-    	    // $('#slider').attr( 'max', Math.round(dur));
-    	    // $('#slider').slider('refresh');
-        // }
-   // }, 100);
+// var loctofl=getPhoneGapPath() + file;
+// audio = new Media(loctofl, function(){ // success callback
+// $('#play').button('enable');
+// console.log("playAudio():Audio Success");
+// }, function(error){ // error callback
+// alert('code: '    + error.code    + '\n' +
+// 'message: ' + error.message + '\n');
+// },function(status){
+// if(status==2)
+// playing=true;
+// else
+// playing=false;
+// });
 
-    
-    // audio.play();
-    
-    // audio.seekTo(pausePos*1000);
 
-    // if (audioTimer == null) {
-        // audioTimer = setInterval(function() {
-            // audio.getCurrentPosition(
-                // function(position) { // get position success
-                    // if (position > -1) {
-                        // setAudioPosition(position);
-                    // }
-                // }, function(e) { // get position error
-                    // console.log("Error getting pos=" + e);
-                // }
-            // );
-        // }, 1000);
-    // }
+// var counter = 0;
+// var timerDur = setInterval(function() {
+// counter = counter + 100;
+// if (counter > 2000) {
+// clearInterval(timerDur);
+// }
+// var dur = audio.getDuration();
+// if (dur > 0) {
+// clearInterval(timerDur);
+// $('#slider').attr( 'max', Math.round(dur));
+// $('#slider').slider('refresh');
+// }
+// }, 100);
+
+
+// audio.play();
+
+// audio.seekTo(pausePos*1000);
+
+// if (audioTimer == null) {
+// audioTimer = setInterval(function() {
+// audio.getCurrentPosition(
+// function(position) { // get position success
+// if (position > -1) {
+// setAudioPosition(position);
+// }
+// }, function(e) { // get position error
+// console.log("Error getting pos=" + e);
+// }
+// );
+// }, 1000);
+// }
 // }
 
 // /* stop audio */
 // function stopAudio() {
-    // if (audio) {
-        // audio.stop();
-        // audio.release();
-    // }
-    // clearInterval(audioTimer);
-    // audioTimer = null;
-    // pausePos = 0;
-    // $('#slider').val(position);
-    // $('#slider').slider('refresh');
+// if (audio) {
+// audio.stop();
+// audio.release();
+// }
+// clearInterval(audioTimer);
+// audioTimer = null;
+// pausePos = 0;
+// $('#slider').val(position);
+// $('#slider').slider('refresh');
 // }
 
 // /* set audio position */
 // function setAudioPosition(position) {
-	// pausePos = position;
-	// position = Math.round(position);
-    // $('#slider').val(position);
-    // $('#slider').slider('refresh');
+// pausePos = position;
+// position = Math.round(position);
+// $('#slider').val(position);
+// $('#slider').slider('refresh');
 // }
-
--- Web App/www/js/mapa.js
+++ Web App/www/js/mapa.js
@@ -1,49 +1,53 @@
-
 var watchID = null;
-var mylat=null;
-var mylng=null;
-var tskloc=null;
-var myloc=null;
-var bimage; 
+var mylat = null;
+var mylng = null;
+var tskloc = null;
+var myloc = null;
+var bimage;
 var gimage;
 var shadow;
 
-$('#page-map').live('pageinit', function(event){
+$('#page-map').live('pageinit', function(event) {
     $('#map_canvas').gmap('addControl', 'controls', google.maps.ControlPosition.BOTTOM_CENTER);
-    $('div#controls').css('display','inline').css('opacity', '0.7');
+    $('div#controls').css('display', 'inline').css('opacity', '0.7');
 
     $('div#controls a#myloc').click(function() {
-    		startwatching();
+        startwatching();
     });
 
-    
 });
-$('#page-map').live('pageshow',function(){
-	if(isconnected)  {
-		//Create blue, green and shadow images for custom markers
-		bimage = new google.maps.MarkerImage('img/markers/bimage.png',
-				new google.maps.Size(20, 34), new google.maps.Point(0,0),
-				new google.maps.Point(10, 34));
+$('#page-map').live('pageshow', function() {
+    if (isconnected) {
+        //Create blue, green and shadow images for custom markers
+        bimage = new google.maps.MarkerImage('img/markers/bimage.png',
+        new google.maps.Size(20, 34), new google.maps.Point(0, 0),
+        new google.maps.Point(10, 34));
 
-		gimage = new google.maps.MarkerImage('img/markers/gimage.png',
-				new google.maps.Size(20, 34), new google.maps.Point(0, 0),
-				new google.maps.Point(10, 34));
+        gimage = new google.maps.MarkerImage('img/markers/gimage.png',
+        new google.maps.Size(20, 34), new google.maps.Point(0, 0),
+        new google.maps.Point(10, 34));
 
-		shadow = new google.maps.MarkerImage('img/markers/shadow.png',
-				new google.maps.Size(40, 34), new google.maps.Point(0, 0),
-				new google.maps.Point(10, 34));
-		}
-var info="Task Location";
-if(window.localStorage.getItem("scriptlat0") && window.localStorage.getItem("scriptlng0"))
-tskloc=new google.maps.LatLng((window.localStorage.getItem("scriptlat0")+0.0),(window.localStorage.getItem("scriptlng0")+0.0));    
-else
-tskloc=new google.maps.LatLng(window.localStorage.getItem("campuslat"),window.localStorage.getItem("campuslng"));    
-$('#map_canvas').gmap('addMarker', {'position': tskloc, 'icon': bimage, 
-    	animation:google.maps.Animation.DROP,'shadow' : shadow, 'shape' : shadowShape, 'bounds': true   } ).click(function(e) {
-        $('#map_canvas').gmap('openInfoWindow', {'content': info}, this); 
+        shadow = new google.maps.MarkerImage('img/markers/shadow.png',
+        new google.maps.Size(40, 34), new google.maps.Point(0, 0),
+        new google.maps.Point(10, 34));
+    }
+    var info = "Task Location";
+    if (window.localStorage.getItem("scriptlat0") && window.localStorage.getItem("scriptlng0")) tskloc = new google.maps.LatLng((window.localStorage.getItem("scriptlat0") + 0.0), (window.localStorage.getItem("scriptlng0") + 0.0));
+    else tskloc = new google.maps.LatLng(window.localStorage.getItem("campuslat"), window.localStorage.getItem("campuslng"));
+    $('#map_canvas').gmap('addMarker', {
+        'position': tskloc,
+        'icon': bimage,
+        animation: google.maps.Animation.DROP,
+        'shadow': shadow,
+        'shape': shadowShape,
+        'bounds': true
+    }).click(function(e) {
+        $('#map_canvas').gmap('openInfoWindow', {
+            'content': info
+        }, this);
     });
 
-	//startwatching();
+    //startwatching();
 });
 
 
@@ -51,112 +55,126 @@ $('#page-map').live("pagehide", function() {
     // Remove all markers when leaving map page - user may edit markers
     $('#map_canvas').gmap('clear', 'markers');
     //google.maps.event.clearInstanceListeners(marker);
-    mylat=null;
-    mylng=null;
+    mylat = null;
+    mylng = null;
     //clearWatch();
 });
 
-//function getroute(){
+//function getroute() {
 //
-//	if(mylat!=null && mylng!=null){
-//		$('#map_canvas').gmap('clear', 'markers');
-//		 $('#map_canvas').gmap('displayDirections', 
-//	              { 'origin' : new google.maps.LatLng(mylat, mylng), 
-//	                'destination' : new google.maps.LatLng(43.676082, -79.410038), 'travelMode' : google.maps.DirectionsTravelMode.DRIVING},
-//	                    function (result, status) {
-//	                        if (status === 'OK') {
-//	                            var center = result.routes[0].bounds.getCenter();
-//	                            $('#map_canvas').gmap('option', 'center', center);
-//	                            $('#map_canvas').gmap('refresh');
-//	                        } else {
-//	                          alert('Unable to get route');
-//	                        }
-//	                    }
-//	       ); 
-//	}else{
-//       alert("Your location has not been found yet!");
-//	}
-//	
+//    if (mylat != null && mylng != null) {
+//        $('#map_canvas').gmap('clear', 'markers');
+//        $('#map_canvas').gmap('displayDirections', {
+//            'origin': new google.maps.LatLng(mylat, mylng),
+//            'destination': new google.maps.LatLng(43.676082, -79.410038),
+//            'travelMode': google.maps.DirectionsTravelMode.DRIVING
+//        },
+//
+//        function(result, status) {
+//            if (status === 'OK') {
+//                var center = result.routes[0].bounds.getCenter();
+//                $('#map_canvas').gmap('option', 'center', center);
+//                $('#map_canvas').gmap('refresh');
+//            } else {
+//                alert('Unable to get route');
+//            }
+//        });
+//    } else {
+//        alert("Your location has not been found yet!");
+//    }
+//
 //}
 
 
-function startwatching(){
-	showloading();
-	  var options = { enableHighAccuracy: true };
-     // watchID = navigator.geolocation.watchPosition(onSuccess, onError, options);
-	  navigator.geolocation.getCurrentPosition(onSuccess, onError, options);
+function startwatching() {
+    showloading();
+    var options = {
+        enableHighAccuracy: true
+    };
+    // watchID = navigator.geolocation.watchPosition(onSuccess, onError, options);
+    navigator.geolocation.getCurrentPosition(onSuccess, onError, options);
 }
 
 
-  // onSuccess Geolocation
-  //
-  function onSuccess(position) {
-	  mylat=position.coords.latitude;
-	  mylng=position.coords.longitude;
-	  myloc=new google.maps.LatLng(mylat, mylng);
-	 var info=getdistance(tskloc.lat(),tskloc.lng(),mylat,mylng).toFixed(3);
-	 if(info<1)
-		 info=(info*1000) + " m";
-	 else
-		 info+=" km"
-      $('#map_canvas').gmap('find', 'markers', { 'property': 'tags', 'value': 'myloc' }, function(marker, isFound) {
-          if ( isFound ) {
-              marker.setMap(null);
-          }
-      });
-	  $('#map_canvas').gmap('addMarker', {'tags': 'myloc','position': myloc, animation:google.maps.Animation.DROP,
-		  'icon': gimage,'shadow' : shadow, 'shape' : shadowShape, 'bounds': true  } ).click(function(e) {
-	        $('#map_canvas').gmap('openInfoWindow', {'content': 'You are '+info+' far from task.'}, this); ///////SET REAL CONTENT HERE
-	    });
-	  hideloading();
-      //clearWatch();
-  }
+// onSuccess Geolocation
+//
 
-  // clear the watch that was started earlier
-  // 
-  function clearWatch() {
-      if (watchID != null) {
-          //navigator.geolocation.clearWatch(watchID);
-    	  //navigator.compass.clearWatch(watchID);
+function onSuccess(position) {
+    mylat = position.coords.latitude;
+    mylng = position.coords.longitude;
+    myloc = new google.maps.LatLng(mylat, mylng);
+    var info = getdistance(tskloc.lat(), tskloc.lng(), mylat, mylng).toFixed(3);
+    if (info < 1) info = (info * 1000) + " m";
+    else info += " km";
+    $('#map_canvas').gmap('find', 'markers', {
+        'property': 'tags',
+        'value': 'myloc'
+    }, function(marker, isFound) {
+        if (isFound) {
+            marker.setMap(null);
+        }
+    });
+    $('#map_canvas').gmap('addMarker', {
+        'tags': 'myloc',
+        'position': myloc,
+        animation: google.maps.Animation.DROP,
+        'icon': gimage,
+        'shadow': shadow,
+        'shape': shadowShape,
+        'bounds': true
+    }).click(function(e) {
+        $('#map_canvas').gmap('openInfoWindow', {
+            'content': 'You are ' + info + ' far from task.'
+        }, this); ///////SET REAL CONTENT HERE
+    });
+    hideloading();
+    //clearWatch();
+}
 
-          watchID = null;
-      }
-  }
-  // onError Callback receives a PositionError object
-  //
-  function onError(error) {
-	  hideloading();
-    alert('code: '    + error.code    + '\n' +
-          'message: ' + error.message + '\n');
-  }
-  
-  
- var shadowShape = {
-  	'type' : 'poly',
-  	'coords' : [ 13, 0, 15, 1, 16, 2, 17, 3, 18, 4, 18, 5, 19, 6, 19, 7, 19, 8,
-  			19, 9, 19, 10, 19, 11, 19, 12, 19, 13, 18, 14, 18, 15, 17, 16, 16,
-  			17, 15, 18, 14, 19, 14, 20, 13, 21, 13, 22, 12, 23, 12, 24, 12, 25,
-  			12, 26, 11, 27, 11, 28, 11, 29, 11, 30, 11, 31, 11, 32, 11, 33, 8,
-  			33, 8, 32, 8, 31, 8, 30, 8, 29, 8, 28, 8, 27, 8, 26, 7, 25, 7, 24,
-  			7, 23, 6, 22, 6, 21, 5, 20, 5, 19, 4, 18, 3, 17, 2, 16, 1, 15, 1,
-  			14, 0, 13, 0, 12, 0, 11, 0, 10, 0, 9, 0, 8, 0, 7, 0, 6, 1, 5, 1, 4,
-  			2, 3, 3, 2, 4, 1, 6, 0, 13, 0 ]
-  };
+// clear the watch that was started earlier
+//
 
-  
-  function getdistance(originlat,originlng,destinationlat,destinationlng){
-	  var R = 6371; // km
-	  var dLat = toRad(destinationlat-originlat);
-	  var dLon = toRad(destinationlng-originlng); 
-	  var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
-	          Math.cos(toRad(originlat)) * Math.cos(toRad(destinationlat)) * 
-	          Math.sin(dLon/2) * Math.sin(dLon/2); 
-	  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-	  var d = R * c;
-	  return d;
-  }
-  
-  function toRad(value){
-	  return value * Math.PI / 180;
-  }
- 
+function clearWatch() {
+    if (watchID !== null) {
+        //navigator.geolocation.clearWatch(watchID);
+        //navigator.compass.clearWatch(watchID);
+
+        watchID = null;
+    }
+}
+// onError Callback receives a PositionError object
+//
+
+function onError(error) {
+    hideloading();
+    alert('code: ' + error.code + '\n' +
+        'message: ' + error.message + '\n');
+}
+
+
+var shadowShape = {
+    'type': 'poly',
+    'coords': [13, 0, 15, 1, 16, 2, 17, 3, 18, 4, 18, 5, 19, 6, 19, 7, 19, 8,
+    19, 9, 19, 10, 19, 11, 19, 12, 19, 13, 18, 14, 18, 15, 17, 16, 16,
+    17, 15, 18, 14, 19, 14, 20, 13, 21, 13, 22, 12, 23, 12, 24, 12, 25,
+    12, 26, 11, 27, 11, 28, 11, 29, 11, 30, 11, 31, 11, 32, 11, 33, 8,
+    33, 8, 32, 8, 31, 8, 30, 8, 29, 8, 28, 8, 27, 8, 26, 7, 25, 7, 24,
+    7, 23, 6, 22, 6, 21, 5, 20, 5, 19, 4, 18, 3, 17, 2, 16, 1, 15, 1,
+    14, 0, 13, 0, 12, 0, 11, 0, 10, 0, 9, 0, 8, 0, 7, 0, 6, 1, 5, 1, 4,
+    2, 3, 3, 2, 4, 1, 6, 0, 13, 0]
+};
+
+
+function getdistance(originlat, originlng, destinationlat, destinationlng) {
+    var R = 6371; // km
+    var dLat = toRad(destinationlat - originlat);
+    var dLon = toRad(destinationlng - originlng);
+    var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.cos(toRad(originlat)) * Math.cos(toRad(destinationlat)) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    var d = R * c;
+    return d;
+}
+
+function toRad(value) {
+    return value * Math.PI / 180;
+}
